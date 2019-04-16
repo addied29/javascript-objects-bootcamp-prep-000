cfa81fc19113422d330ed11f086009b57a656e07
Addie Dean <addiedean@gmail.com>
2019-04-16 23:47:42 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,6 +6,8 @@ function updatePlaylist(playlist,artistName,songTitle){
 }
 
 function removeFromPlaylist(playlist,artistName){
+  playlist[artistName]=songTitle
   delete playlist.artistName;
-  playlist;
+  return playlist
+  
 }
