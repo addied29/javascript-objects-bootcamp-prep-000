8a119d96242adaa87f1e5209f79c1786e4cbaebb
Addie Dean <addiedean@gmail.com>
2019-04-16 23:44:16 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {artistName:"songTitle"}
 
 function updatePlaylist(playlist,artistName,songTitle){
-  playlist[artistName]=
+  playlist[artistName]=songTitle
   return Object.assign({},playlist,{[artistName]:songTitle})
   
   updatePlaylist(playlist,'Slowdive','Alison')
