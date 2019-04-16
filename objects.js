ac4215a5526e0954c62825fdd2e8f8854d52d46e
Addie Dean <addiedean@gmail.com>
2019-04-16 23:44:45 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,6 +4,5 @@ function updatePlaylist(playlist,artistName,songTitle){
   playlist[artistName]=songTitle
   return Object.assign({},playlist,{[artistName]:songTitle})
   
-  updatePlaylist(playlist,'Slowdive','Alison')
 }
 
